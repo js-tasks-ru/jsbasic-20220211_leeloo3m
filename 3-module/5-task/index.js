715645dc6b4fe2980caee73735f230323263f080
Elena Medvedeva <leeloo3M@yandex.ru>
2022-02-15 15:32:38 +0300

    3.5 done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let result = str.split(' ');
+  let result2 = result.filter(item => item == +item);
+  let resultmax = Math.max.apply(null, result2);
+  let resultmin = Math.min.apply(null, result2);
+  let f = {
+    min: resultmin,
+    max: resultmax
+  }
+  return f;
 }
