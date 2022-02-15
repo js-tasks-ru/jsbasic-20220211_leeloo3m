e2b2e9ac5d714012524b79eaa372789e91587576
Elena Medvedeva <leeloo3M@yandex.ru>
2022-02-15 13:06:28 +0300

    all that delete

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
+const { sum, multiply } = require("lodash");
+
 let calculator = {
-  // ваш код
+ read(a, b) {
+   this.a = +a;
+   this.b = +b;
+ },
+ sum() {
+   return this.a + this.b;
+ },
+ mul() {
+   return this.a * this.b;
+ }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
