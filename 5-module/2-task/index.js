a06a7feeaea02c6eb31a2c77451d256fd6fc4e23
Elena Medvedeva <leeloo3M@yandex.ru>
2022-02-21 15:22:07 +0300

    two easy task from 5 modul

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let div = document.querySelector('#text');
+  
+  return btn.addEventListener('click', () => {
+    if(div.hasAttribute('hidden')) {
+      div.removeAttribute('hidden')
+    } else {
+      div.setAttribute('hidden', 'true');
+    }
+  })
 }
