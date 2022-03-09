111f9f87c95bb7cf1e0fb568f5d25dd372800a2b
Elena Medvedeva <leeloo3M@yandex.ru>
2022-03-09 15:02:25 +0300

    5.3.1

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let carousel = document.querySelector('.carousel__inner');
+  let offsetWidth = carousel.offsetWidth;
+  let startPosition = 0;
+  
+
+  arrowLeft.addEventListener('click', () => {
+    startPosition+=1;
+    carousel.style.transform = 'translateX('+ (-offsetWidth * startPosition) + 'px)';
+    buttons();
+})
+
+  arrowRight.addEventListener('click', () => {
+    startPosition-=1;
+    carousel.style.transform = 'translateX(' +(-offsetWidth * startPosition) + 'px)';
+    buttons();
+  })
+
+  const buttons = () => {
+
+    if(startPosition==3) {
+    arrowLeft.style.display = 'none';
+  } else {
+    arrowLeft.style.display = '';
+  }
+
+  if(startPosition==0) {
+    arrowRight.style.display = 'none';
+  } else {
+    arrowRight.style.display = '';
+  }
+  
+}
+
+buttons();
+
+
 }
