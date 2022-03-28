14155f3c1555ca5ffea6444d0a5c7c08fd60a22c
Elena Medvedeva <leeloo3M@yandex.ru>
2022-03-28 22:13:53 +0300

    8.3

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -2,27 +2,84 @@ export default class Cart {
   cartItems = []; // [product: {...}, count: N]
 
   constructor(cartIcon) {
+    
     this.cartIcon = cartIcon;
+    this.addProduct();
+    this.updateProductCount();
+    this.isEmpty();
+    this.getTotalCount();
+    this.getTotalPrice();
   }
 
   addProduct(product) {
-    // ваш код
+    let foundProduct = this.cartItems.some(item=>item.id==product.id);
+    let obj = {};
+    obj.product=product;
+    obj.count = 1;
+
+    if(product==null||product.length==0||product==undefined){
+      try {
+        this.addProduct();
+      }
+      catch (e) {
+        //error
+      }
+      return;
+    }else if(foundProduct==false){
+      this.cartItems.push(obj);
+    } else if(foundProduct==true) {
+      let cart = this.cartItems.find(cart=>cart.id==product.id);
+      cart.count++;
+    }
+    
+   this.onProductUpdate(this.cartItems);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+   
+    this.cartItems.forEach(cart=>{
+      if(cart.product.id==productId){
+        if(amount==1){
+          cart.count++;
+
+        } else if(amount==-1){
+          cart.count--;
+          if(cart.count==0){
+            let i = this.cartItems.indexOf(cart);
+            this.cartItems.splice(i, 1);
+          }
+        }
+      }
+      
+    })
+    this.onProductUpdate(this.cartItems);
+    //console.log(this.cartItems);
   }
 
   isEmpty() {
-    // ваш код
+    // Возвращает true если корзина пустая и false если в корзине есть хотя бы один товар.
+    if(this.cartItems.length==0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getTotalCount() {
-    // ваш код
+    // Возвращает общее количество товаров в корзине. Обратите внимание, что один товар может быть добавлен несколько раз и это нужно учесть.
+      let totalCount = 0;
+     this.cartItems.forEach(cart=>{
+       return totalCount+=cart.count;
+     })
+     //console.log(this.cartItems)
   }
 
   getTotalPrice() {
-    // ваш код
+    //Возвращает стоимость всех товаров в корзине. Для этого нужно сложить все цены товаров с учетом количества каждого из них. Цену товара можно найти в свойстве price объекта товара.
+    let totalPrice = 0;
+    this.cartItems.forEach(cart=>{
+      return totalPrice+=cart.count*cart.product.price;
+    })
   }
 
   onProductUpdate(cartItem) {
